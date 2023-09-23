bd78d88e443d71a991dd2bfcaf0fe2675870f1d3
zer0-K <adrienkaufman@gmail.com>
2023-09-23 17:35:23 +0200

    fix quine after halt index change

--- src/Experiment/BioEvo/Replicators/EvoAlgos/EvoX/ReplicationEvoXQuine.cpp
+++ src/Experiment/BioEvo/Replicators/EvoAlgos/EvoX/ReplicationEvoXQuine.cpp
@@ -40,34 +40,35 @@ void ReplicationEvoXQuine::launch()
         instruction::ADD, 0, 0, 0, 2, 2, 2,     // 8 (d[2] = 16)
         instruction::ADD, 0, 0, 0, 2, 2, 3,     // 9 (d[2] = 20)
         instruction::ADD, 0, 0, 0, 2, 2, 4,     // 10 (d[2] = 28)
-        instruction::DEC, 0, 0, 0, 3, 0, 0,     // 11 (d[3] = 3)
-        instruction::INC, 0, 0, 0, 6, 0, 0,     // 12 (d[6] = 1)
-        instruction::INC, 0, 0, 0, 6, 0, 0,     // 13 (d[6] = 2)
-        instruction::DEC, 0, 0, 0, 7, 0, 0,     // 14 (d[7] = -1)
-        instruction::INC, 0, 0, 0, 7, 0, 0,     // 15 (d[7]++ (i))
-        instruction::GR, 0, 1, 0, 5, 7, 0,      // 16 (d[5] = gene at i)
-        instruction::JRE, 0, 0, 0, 6, 2, 5,     // 17
-        instruction::JRS, 0, 0, 0, 3, 0, 0,     // 18
-        instruction::ADD, 0, 0, 0, 7, 7, 4,     // 19
-        instruction::DEC, 0, 0, 0, 7, 0, 0,     // 20
-        instruction::DEC, 0, 0, 0, 8, 0, 0,     // 21 (j)
-        instruction::SETOS, 0, 0, 0, 7, 0, 0,   // 22
-        instruction::INC, 0, 0, 0, 3, 0, 0,     // 23 (d[3] = 4)
-        instruction::INC, 0, 0, 0, 8, 0, 0,     // 24
-        instruction::GR, 0, 1, 0, 5, 8, 0,      // 25
-        instruction::CPYOUT, 1, 0, 0, 8, 5, 0,  // 26
-        instruction::JRE, 0, 0, 0, 6, 7, 8,     // 27
-        instruction::JRS, 0, 0, 0, 3, 0, 0,     // 28
-        instruction::HALT, 0, 0, 0, 0, 0, 0     // 29
+        instruction::INC, 0, 0, 0, 2, 0, 0,     // 11 (d[2] = 29)
+        instruction::DEC, 0, 0, 0, 3, 0, 0,     // 12 (d[3] = 3)
+        instruction::INC, 0, 0, 0, 6, 0, 0,     // 13 (d[6] = 1)
+        instruction::INC, 0, 0, 0, 6, 0, 0,     // 14 (d[6] = 2)
+        instruction::DEC, 0, 0, 0, 7, 0, 0,     // 15 (d[7] = -1)
+        instruction::INC, 0, 0, 0, 7, 0, 0,     // 16 (d[7]++ (i))
+        instruction::GR, 0, 1, 0, 5, 7, 0,      // 17 (d[5] = gene at i)
+        instruction::JRE, 0, 0, 0, 6, 2, 5,     // 18
+        instruction::JRS, 0, 0, 0, 3, 0, 0,     // 19
+        instruction::ADD, 0, 0, 0, 7, 7, 4,     // 20
+        instruction::DEC, 0, 0, 0, 7, 0, 0,     // 21
+        instruction::DEC, 0, 0, 0, 8, 0, 0,     // 22 (j)
+        instruction::SETOS, 0, 0, 0, 7, 0, 0,   // 23
+        instruction::INC, 0, 0, 0, 3, 0, 0,     // 24 (d[3] = 4)
+        instruction::INC, 0, 0, 0, 8, 0, 0,     // 25
+        instruction::GR, 0, 1, 0, 5, 8, 0,      // 26
+        instruction::CPYOUT, 1, 0, 0, 8, 5, 0,  // 27
+        instruction::JRE, 0, 0, 0, 6, 7, 8,     // 28
+        instruction::JRS, 0, 0, 0, 3, 0, 0,     // 29
+        instruction::HALT, 0, 0, 0, 0, 0, 0     // 30
     };
     /*
         0-1 : dummy input (as 'template'), useless here
-        2-10 : create the number 28 (and save other numbers)
-        11-14 : builds some variables for jumps (loops)
-        15-18 :first loop : identify length of the genome
-        19-23 : builds some vars for the next loop, and change output size
-        24-28 : second loop (copying genome into output)
-        29 : end
+        2-11 : create the number 28 (and save other numbers)
+        12-15 : builds some variables for jumps (loops)
+        16-19 :first loop : identify length of the genome
+        20-24 : builds some vars for the next loop, and change output size
+        25-20 : second loop (copying genome into output)
+        30 : end
     */
 
     std::vector<std::vector<int>> genomes {
