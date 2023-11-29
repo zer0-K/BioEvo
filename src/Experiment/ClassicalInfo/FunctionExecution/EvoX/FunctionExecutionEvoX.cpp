e62d9f6cbfccc8f2b9711017ea72cef504a814ee
adrien <adrienkaufman@gmail.com>
2023-11-29 15:40:30 +0100

    EvoX JMP01 arch CALL on multilevel func exec

--- src/Experiment/ClassicalInfo/FunctionExecution/EvoX/FunctionExecutionEvoX.cpp
+++ src/Experiment/ClassicalInfo/FunctionExecution/EvoX/FunctionExecutionEvoX.cpp
@@ -357,9 +357,109 @@ void FunctionExecutionEvoX::launch()
         instruction::MARKER, 0, 5, 0, 0, 0, 0 
     };
 
+    /**
+     * Multi level short call function : uses the 'CALL' instruction
+     */
+    std::vector<int> genome_multilevel_call {
+        instruction::JMP, 0, 0, 0, 25, 0, 0,    // skip meta-exec func
+
+    //----- exec fct with given id
+        // args :
+        //      1 - func id
+        instruction::MARKER, 0, 0, 0, 0, 0, 0,
+        
+        // find marker with given id
+        instruction::CPY, 1, 2, 1, 100, 99, 0,  // get arg 1 from stack
+        instruction::CPY, 1, 0, 0, 110, -7, 0,
+        instruction::ADD, 1, 1, 0, 110, 110, 7,
+        instruction::GR, 1, 2, 0, 101, 110, 0,
+        instruction::JRE, 0, 1, 0, 2, 101, instruction::MARKER, // marker found
+        instruction::JRS, 0, 0, 0, 3, 0, 0,
+        // check func id
+        instruction::ADD, 1, 1, 0, 110, 110, 2,
+        instruction::GR, 1, 2, 0, 101, 110, 0,
+        instruction::JRE, 0, 1, 1, 3, 101, 100,
+        instruction::SUB, 1, 1, 0, 110, 110, 2,
+        instruction::JRS, 0, 0, 0, 8, 0, 0,
+
+        // update data stack (args of func to exec are just before the func id)
+        instruction::DEC, 1, 0, 0, 99, 0, 0,
+
+        // set prog ptr for exec
+        instruction::DIV, 1, 1, 0, 110, 110, 7,
+        instruction::INC, 1, 0, 0, 0, 0, 0,
+        instruction::GPTR, 2, 0, 0, 0, 0, 0,    // update prog ptr stack
+        instruction::ADD, 2, 2, 0, 0, 0, 3,
+        instruction::JMP, 1, 0, 0, 110, 0, 0,
+
+        // clean the stacks
+        instruction::DEC, 1, 0, 0, 0, 0, 0,     // update prog ptr stack
+        instruction::CPY, 1, 2, 0, 103, 0, 0,
+        instruction::DEC, 1, 0, 0, 0, 0, 0,
+        instruction::JMP, 1, 0, 0, 103, 0, 0,
+
+        instruction::MARKER, 0, 0, 0, 0, 0, 0,
+
+    //----- main func
+        instruction::MARKER, 0, 1, 0, 0, 0, 0,
+
+        instruction::CPY, 1, 0, 0, 99, 130, 0,  // data stack begins at 130
+        // call muti-level exec func
+        instruction::CALL, 0, 0, 0, 1, 2, 0,
+        // end
+        instruction::HALT, 0, 0, 0, 0, 0, 0,
+
+        instruction::MARKER, 0, 1, 0, 0, 0, 0,
+
+    //----- function 'main' calls
+        instruction::MARKER, 0, 2, 0, 0, 0, 0,
+
+        // call func 1
+        instruction::CALL, 0, 0, 0, 0, 3, 0,
+        // arg1 = return of func 1, build arg2 (arg2 = 4)
+        instruction::INC, 1, 0, 0, 99, 0, 0,
+        instruction::CPY, 2, 0, 0, 99, 4, 0,
+        // call func 2
+        instruction::CALL, 0, 0, 0, 0, 4, 0,
+        // call func 3
+        instruction::CALL, 0, 0, 0, 0, 5, 0,
+
+        instruction::JMP, 2, 0, 0, 0, 0, 0,
+        instruction::MARKER, 0, 2, 0, 0, 0, 0,
+ 
+
+    //----- first func : get input
+        instruction::MARKER, 0, 3, 0, 0, 0, 0,
+
+        instruction::CPYIN, 1, 1, 0, 100, 0, 0,     // get input value
+        instruction::INC, 1, 0, 0, 99, 0, 0,        // increment stack for return value
+        instruction::CPY, 2, 1, 0, 99, 100, 0,      // store return value at top of stack
+
+        instruction::JMP, 2, 0, 0, 0, 0, 0,
+        instruction::MARKER, 0, 3, 0, 0, 0, 0,
+ 
+    //----- second func : return = arg1 + arg2
+        instruction::MARKER, 0, 4, 0, 0, 0, 0,
+
+        instruction::CPY, 1, 2, 0, 100, 99, 0,        
+        instruction::DEC, 1, 0, 0, 99, 0, 0,        // remove second arg from stack
+        instruction::ADD, 2, 2, 1, 99, 99, 100,     // return = arg1 + arg2
+
+        instruction::JMP, 2, 0, 0, 0, 0, 0,
+        instruction::MARKER, 0, 4, 0, 0, 0, 0,
+ 
+    //----- third func : copy arg to output
+        instruction::MARKER, 0, 5, 0, 0, 0, 0,
+
+        instruction::CPYOUT, 1, 2, 0, 0, 99, 0,     // copy arg to out
+
+        instruction::JMP, 2, 0, 0, 0, 0, 0,
+        instruction::MARKER, 0, 5, 0, 0, 0, 0 
+    };
+
 
     std::vector<std::vector<int>> genomes {
-        genome_simple, genome_multilevel, genome_memory
+        genome_simple, genome_multilevel, genome_memory, genome_multilevel_call
     };
 
 
@@ -387,9 +487,10 @@ void FunctionExecutionEvoX::launch()
         input_3_1, input_3_2, input_3_3
     };
 
-
+    std::vector<std::vector<int>> inputs_4 = inputs_2;
+ 
     std::vector<std::vector<std::vector<int>>> inputs {
-        inputs_1, inputs_2, inputs_3
+        inputs_1, inputs_2, inputs_3, inputs_4
     };
 
        
